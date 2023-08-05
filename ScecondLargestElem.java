90078dee1847386a2cd887a8176b88a37f96bdb5
arkopravo <arka.bhuin@gmail.com>
2023-08-05 14:17:45 +0530

    Fifthcommit

--- ScecondLargestElem.java
+++ ScecondLargestElem.java
@@ -1,6 +1,8 @@
 package DsaLevel1;
 import  java.util.*;
 public class ScecondLargestElem {
+
+
     static  int findSecondLarge(int arr[] , int n){
         n = arr.length;
         int largest = arr[0];
@@ -16,6 +18,24 @@ public class ScecondLargestElem {
             }
         }
         return secondLarge;
+
+    }
+    static  int getSecondSmallest(int arr[] , int n){
+        n = arr.length;
+        int smlarge = arr[0];
+        int ssmall = -1;
+        for(int i = 0 ; i< n ;i++){
+            if(arr[i] < smlarge){
+                smlarge =ssmall;
+                ssmall = arr[i];
+
+            }
+            else if(arr[i] !=smlarge && arr[i] >ssmall){
+                 ssmall =arr[i];
+
+            }
+        }
+        return ssmall;
     }
     public static void main(String[] args) {
        Scanner inp = new Scanner(System.in);
@@ -33,5 +53,7 @@ public class ScecondLargestElem {
 
        int result = findSecondLarge(arr,b);
         System.out.println("the second largest element in array is : " +  result);
+        int c = getSecondSmallest(arr, b);
+        System.out.println(c);
     }
 }
